5220d471da1015fe990fe32c058c8b3b75be6fda
Les Matheson <les.matheson@gmail.com>
2018-01-20 08:57:21 -0500

    sync

---
+++ hello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Remove 2.x references in PYTHONPATH!
+# Install python-docx with pip (pip3 install --pre python-docx)
+
+import docx
+
+
+if __name__=="__main__":
+
+    print( "Hello world")
